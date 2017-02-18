18e86cc98ef336035aed969c9ec2877e79a5a296
cararey <carareynolds13@knights.ucf.edu>
2017-02-18 15:31:31 -0500

    with animate.css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
 console.log('\'Allo \'Allo!');
+
+$('#hello').addClass('animated tada');
+$('#tagline').addClass('animated fadeIn');
+$('#hireme').addClass('animated pulse');
+$('#hello').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
