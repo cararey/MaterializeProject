f8dd2f6ca16cd58bb65d644e01a71517dbd87173
cararey <carareynolds13@knights.ucf.edu>
2017-02-25 21:53:03 -0500

    materializetest

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,5 @@ $('#hello').addClass('animated tada');
 $('#tagline').addClass('animated fadeIn');
 $('#hireme').addClass('animated pulse');
 $('#hello').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
+
+
