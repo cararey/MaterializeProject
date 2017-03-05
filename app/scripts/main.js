edf69be68d395d6d453c92a813668d9a920ec661
cararey <carareynolds13@knights.ucf.edu>
2017-03-05 11:46:42 -0500

    pages commit

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+console.log('\'Allo \'Allo!');
+
+$('#hello').addClass('animated tada');
+$('#tagline').addClass('animated fadeIn');
+$('#hireme').addClass('animated pulse');
+$('#hello').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
+
+
