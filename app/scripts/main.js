ed91e9cc49af0fb93dea321ef857205249e7ef17
cararey <carareynolds13@knights.ucf.edu>
2017-03-05 11:47:25 -0500

    Update 2017-03-05T16:47:22.559Z

--- app/scripts/main.js
+++
@@ -1,8 +0,0 @@
-console.log('\'Allo \'Allo!');
-
-$('#hello').addClass('animated tada');
-$('#tagline').addClass('animated fadeIn');
-$('#hireme').addClass('animated pulse');
-$('#hello').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
-
-
---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$(".collapsible").collapsible()}),$("#hello").addClass("animated tada"),$("#tagline").addClass("animated fadeIn"),$("#hireme").addClass("animated pulse"),$("#hello").one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",doSomething);
