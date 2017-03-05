f0ce86c6559e55aa4efcee5020f9c4042bc9eb91
cararey <carareynolds13@knights.ucf.edu>
2017-03-05 11:50:14 -0500

    Updated Animation and Styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,12 @@
 console.log('\'Allo \'Allo!');
 
+
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
 $('#hello').addClass('animated tada');
 $('#tagline').addClass('animated fadeIn');
 $('#hireme').addClass('animated pulse');
 $('#hello').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
-
-
