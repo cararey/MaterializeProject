bb90719346e772aaad9ce7ad9543eb13dce802bb
cararey <carareynolds13@knights.ucf.edu>
2017-03-05 13:30:43 -0500

    fixing js and upload issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,5 +11,4 @@ $(document).ready(function(){
 $('#hello').addClass('animated tada');
 $('#tagline').addClass('animated fadeIn');
 $('#hireme').addClass('animated pulse');
-$('#hello').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething);
 
